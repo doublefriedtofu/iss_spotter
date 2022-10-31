97ff841b6f494c0fa6bef37ce957b8f374ed69a0
doublefriedtofu <hurdonghyuk@gmail.com>
2022-10-31 08:53:10 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // index.js
 
 // The code below is temporary and can be commented out.
-const { fetchMyIP } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
+};
 
-  console.log('It worked! Returned IP:' , ip);
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
+
+
+
+module.exports = { printPassTimes }
+// 99.252.44.72
